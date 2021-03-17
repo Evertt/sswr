17097303c2de72bda9b78ca32bf7b9a64d99f3e5
Evert van Brussel <ecvanbrussel@gmail.com>
2021-03-17 11:29:57 +0100

    remove logging again

--- src/cache.ts
+++ src/cache.ts
@@ -31,7 +31,6 @@ export class SessionCache extends DefaultCache {
   }
 
   private purge() {
-    console.log("purging...")
     this.elements.forEach((cacheItem, key) => {
       if (cacheItem.hasExpired()) {
         this.remove(key, { broadcast: false })
